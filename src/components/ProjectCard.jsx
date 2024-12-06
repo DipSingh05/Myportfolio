222677961e213d584d3878e3ad9f53650c535c9f
DipSingh05 <diptomansingh.personal@gmail.com>
2024-12-06 14:47:12 +0530

    new

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -39,7 +39,7 @@ function ProjectCard({
       <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-orange-400"></div>
       <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-green-200"></div>
     </div>
-    <p className="text-center ml-3 text-[#16f2b3] text-base lg:text-xl font-cyberpunk color-sunset">
+    <p className="text-center ml-3 text-[#16f2b3] text-base text-sm lg:text-xl font-cyberpunk color-sunset">
       {name}
     </p>
   </div>
